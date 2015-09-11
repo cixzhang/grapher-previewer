4bd25114d712ecd7b3f464f6caa0c875c8e2d1ba
Cindy Zhang <cindy.zhang@ayasdi.com>
2015-09-11 10:17:30 -0700

    rebuild

--- index.js
+++ index.js
@@ -4,7 +4,19 @@ var Grapher = require('grapher');
 require('grapher-center')(Grapher);
 require('grapher-target')(Grapher);
 
-var Previewer = {};
+var Previewer = {
+  initialize: function () {
+    this.viewport = document.body;
+    this.grapher = new Grapher();
+    this.canvas = this.grapher.canvas;
+
+    this.textarea = document.createElement('textarea');
+
+
+    this.viewport.appendChild(this.canvas);
+    this.viewport.appendChild(this.textarea);
+  }
+};
 
 module.exports = Previewer;
 
